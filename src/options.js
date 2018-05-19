323ad34eb4ef130afceb59bdb989b35ed97314df
ENT8R <info.ent8r@gmail.com>
2018-05-19 16:23:33 +0200

    Fix suggestions by @rugk

--- src/options.js
+++ src/options.js
@@ -266,15 +266,18 @@ const OptionHandler = (function () {
      * @function
      * @private
      * @param  {string} hex
-     * @returns {Array}
+     * @returns {Array|null}
      */
     function hexToRgb(hex) {
         const result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
-        return result ? [
-            parseInt(result[1], 16),
-            parseInt(result[2], 16),
-            parseInt(result[3], 16)
-        ] : null;
+        if (result) {
+            return [
+                parseInt(result[1], 16),
+                parseInt(result[2], 16),
+                parseInt(result[3], 16)
+            ];
+        }
+        return null;
     }
 
     /**
