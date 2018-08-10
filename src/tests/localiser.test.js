177e9c4ecf9ae0449a61902bc40fce037f10a373
rugk <rugk@posteo.de>
2018-08-11 01:03:52 +0200

    Update Sinon

--- src/tests/localiser.test.js
+++ src/tests/localiser.test.js
@@ -1,6 +1,6 @@
 import "https://unpkg.com/mocha@5.2.0/mocha.js"; /* globals mocha */
 import "https://unpkg.com/chai@4.1.2/chai.js"; /* globals chai */
-import "https://unpkg.com/sinon@6.1.4/pkg/sinon.js"; /* globals sinon */
+import "https://unpkg.com/sinon@6.1.5/pkg/sinon.js"; /* globals sinon */
 
 import * as HtmlMock from "./htmlMock.js";
 
