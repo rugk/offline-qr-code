40e72f395bc2820053439caa7385261b42cd1d67
rugk <rugk@posteo.de>
2018-08-14 14:09:38 +0200

    Add message level tests

--- src/tests/run.js
+++ src/tests/run.js
@@ -3,6 +3,7 @@ import "https://unpkg.com/mocha@5.2.0/mocha.js"; /* globals mocha */
 /* tests */
 import "./selftest.test.js";
 import "./globalConsts.test.js";
+import "./messageLevel.test.js";
 import "./colors.test.js";
 import "./iconHandler.test.js";
 import "./localiser.test.js";
