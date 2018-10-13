ea42948e1b03b28c072bfab077272e16a6642979
rugk <rugk@posteo.de>
2018-10-13 15:08:08 +0200

    Fix EsLint error

--- src/options/modules/AutomaticSettings/internal/LoadAndSave.js
+++ src/options/modules/AutomaticSettings/internal/LoadAndSave.js
@@ -236,24 +236,22 @@ async function resetOptions(event) {
     });
 
     // finally reset options
-    browser.storage.sync.clear().then(() => {
-        return loadAllOptions().then(() => {
-            MessageHandler.showSuccess("resettingOptionsWorked", true, {
-                text: "messageUndoButton",
-                action: () => {
-                    browser.storage.sync.set(lastOptionsBeforeReset).then(() => {
-                        // re-load the options again
-                        loadAllOptions();
-                    }).catch((error) => {
-                        Logger.logError("Could not undo option resetting: ", error);
-                        MessageHandler.showError("couldNotUndoAction");
-                    }).finally(() => {
-                        MessageHandler.hideSuccess();
-                    });
-                }
-            });
-        });
-    }).catch((error) => {
+    browser.storage.sync.clear().then(() => loadAllOptions().then(
+        () => MessageHandler.showSuccess("resettingOptionsWorked", true, {
+            text: "messageUndoButton",
+            action: () => {
+                browser.storage.sync.set(lastOptionsBeforeReset).then(() => {
+                    // re-load the options again
+                    loadAllOptions();
+                }).catch((error) => {
+                    Logger.logError("Could not undo option resetting: ", error);
+                    MessageHandler.showError("couldNotUndoAction");
+                }).finally(() => {
+                    MessageHandler.hideSuccess();
+                });
+            }
+        })
+    )).catch((error) => {
         Logger.logError(error);
         MessageHandler.showError("resettingOptionsFailed", true);
     }).finally(() => {
@@ -290,7 +288,7 @@ export function init() {
         currentElem.addEventListener("change", Trigger.runHtmlEventTrigger);
     });
 
-    const resetButton = document.getElementById("resetButton")
+    const resetButton = document.getElementById("resetButton");
     if (resetButton) {
         resetButton.addEventListener("click", resetOptions);
     }
