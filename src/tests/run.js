65eee09a6ca5b1fdf88cdf80ee6764666762e240
rugk <rugk@posteo.de>
2018-11-19 09:30:08 +0100

    Remove unit tests split into TionyWebExt

--- src/tests/run.js
+++ src/tests/run.js
@@ -7,12 +7,6 @@ import "./dataTest/tips.test.js";
 import "./dataTest/defaultSettings.test.js";
 import "./colors.test.js";
 import "./iconHandler.test.js";
-import "./localiser.test.js";
-import "./addonSettings.test.js";
-import "./logger.test.js";
-import "./messageHandler.test.js";
-import "./randomTips.test.js";
-import "./automaticSettings.test.js";
 
 mocha.checkLeaks();
 mocha.run();
