5286c17e2abf07f35c74281f6b6beb117d5bd5e1
rugk <rugk@posteo.de>
2019-01-11 10:37:14 +0100

    Use defaultSettings test from AddonSettings module instead of bundling it

--- src/tests/run.js
+++ src/tests/run.js
@@ -4,7 +4,7 @@ import "https://unpkg.com/mocha@5.2.0/mocha.js"; /* globals mocha */
 import "./dataTest/globalConsts.test.js";
 import "./dataTest/messageLevel.test.js";
 import "./dataTest/tips.test.js";
-import "./dataTest/defaultSettings.test.js";
+import "/common/modules/AddonSettings/tests/helper/dataTest/defaultSettings.test.js";
 import "./colors.test.js";
 import "./iconHandler.test.js";
 
