a3743297a8c7810f7e52ee6c3603218bb77ccda0
Samuel Sepesi <15359784+daaAd1@users.noreply.github.com>
2019-10-02 23:15:53 +0200

    Refactor random tips

--- src/options/options.js
+++ src/options/options.js
@@ -5,7 +5,6 @@
  * @requires modules/OptionHandler
  */
 
-import { tips } from "/common/modules/data/Tips.js";
 import * as RandomTips from "/common/modules/RandomTips/RandomTips.js";
 import * as AddonSettings from "/common/modules/AddonSettings/AddonSettings.js";
 import * as AutomaticSettings from "/common/modules/AutomaticSettings/AutomaticSettings.js";
@@ -16,7 +15,7 @@ import * as CustomOptionTriggers from "./modules/CustomOptionTriggers.js";
 CustomOptionTriggers.registerTrigger();
 AutomaticSettings.setDefaultOptionProvider(AddonSettings.getDefaultValue);
 AutomaticSettings.init();
-RandomTips.init(tips).then(() => {
+RandomTips.init().then(() => {
     RandomTips.setContext("options");
     RandomTips.showRandomTipIfWanted();
 });
--- src/popup/qrcode.js
+++ src/popup/qrcode.js
@@ -7,12 +7,11 @@
  */
 "use strict";
 
-import { tips } from "/common/modules/data/Tips.js";
 import * as RandomTips from "/common/modules/RandomTips/RandomTips.js";
 
 import "./modules/InitQrCode.js";
 
-RandomTips.init(tips).then(() => {
+RandomTips.init().then(() => {
     RandomTips.setContext("popup");
     RandomTips.showRandomTipIfWanted();
 });
