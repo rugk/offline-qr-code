fe94c10a8cd567c798f2de642aaca97c2449eaa7
DovydasGrb1 <dovydasgrabauskas@gmail.com>
2025-04-09 13:32:26 +0200

    Fix issue #308: added functionality where a user can select whether he wants to see random tips

--- src/popup/qrcode.js
+++ src/popup/qrcode.js
@@ -1,18 +1,52 @@
-/**
- * Starter module for QR code popup.
- *
- * @module qrcode
- * @requires modules/RandomTips
- * @requires modules/InitQrCode
- */
-"use strict";
+// /**
+//  * Starter module for QR code popup.
+//  *
+//  * @module qrcode
+//  * @requires modules/RandomTips
+//  * @requires modules/InitQrCode
+//  */
+// "use strict";
 
-import { tips } from "/common/modules/data/Tips.js";
-import * as RandomTips from "/common/modules/RandomTips/RandomTips.js";
+// import { tips } from "/common/modules/data/Tips.js";
+// import * as RandomTips from "/common/modules/RandomTips/RandomTips.js";
 
-import "./modules/InitQrCode.js";
+// import "./modules/InitQrCode.js";
 
-RandomTips.init(tips).then(() => {
-    RandomTips.setContext("popup");
-    RandomTips.showRandomTipIfWanted();
-});
+const CHECKBOX_ID = "disable-tips-checkbox";
+
+/*Check browser's storage if user wants to receive the tips (default = true)*/
+async function ShowTips() {
+    const result = await browser.storage.local.get("showTips");
+    return result.showTips !== false;
+}
+
+// Save user preference when checkbox is changed
+function setupCheckbox() {
+    const checkbox = document.getElementById(CHECKBOX_ID);
+    if (!checkbox) return;
+
+    // Load initial state
+    browser.storage.local.get("showTips").then((result) => {
+        checkbox.checked = result.showTips === false;
+    });
+
+    // Listen for changes
+    checkbox.addEventListener("change", () => {
+        const show = !checkbox.checked;
+        browser.storage.local.set({ showTips: show });
+    });
+}
+
+async function init() 
+{
+    setupCheckbox();
+    if (await shouldShowTips()) 
+    {
+        RandomTips.init(tips).then(() => {
+            RandomTips.setContext("popup");
+            RandomTips.showRandomTipIfWanted();
+        });
+    }
+}
+
+init();
